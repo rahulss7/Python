c849d22f95ab99a0ba51a3a4dc049c725618a2e6
Rahul Sudke <rahulsudke1@gmail.com>
2023-09-05 00:11:29 +0530

    Add files via upload

---
+++ Num.py
@@ -0,0 +1,86 @@
+
+# Number of Prog 
+ram = 1 == 1
+if ram:
+    print("True")
+else:
+    print("False")
+
+
+
+yogesh  = 1 == 12
+if yogesh:
+    print("True")
+else:
+    print("False")
+
+# Number of prog that multiplication is right or wrong :
+
+rajesh = 12 * 12 
+if rajesh ==  144 :
+    print("True")
+else:
+    print("False")
+
+ram1 = 12 * 13
+if ram1 == 158:
+    print("True")
+else:
+    print("False")
+
+
+
+ram2 = 11 / 12
+if ram2 == 141:
+    print("True")
+else:
+    print("False")
+
+
+ram3 = 11 / 12
+
+if ram2 == 141:
+    print("True")
+else:
+    print(f"False. The correct answer is {ram2}.")
+
+ram4 = 15 / 12
+if ram4 == 145:
+    print("True")
+else:
+    print(f"this is wrong answer. /n The correct answer is {ram4}")
+
+
+
+# WORD 
+
+
+ramesh = "value"
+ram = "value1"
+
+yog = ramesh == ram
+    
+if yog:
+ print("True")
+else:
+ print("False")
+
+raj = "value"
+ram = raj == raj
+if ram == raj:
+   print("True")
+else:
+  print(f"this is wrong. /n The right answer is {ram}") 
+
+
+
+raj = "value"
+rajesh = "value1"
+ram = raj == rajesh
+
+if ram:
+    print("True")
+else:
+    print(f"this is wrong. /n The right answer is {ram}")
+
+
